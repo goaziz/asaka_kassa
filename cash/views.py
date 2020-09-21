d1d9a369522bc7d7f9ff70ee7f13234047d5b5e9
Azizbek Gaybullaev <azizgaybullaev@mail.ru>
2020-09-21 10:48:11 +0500

    modfiied

--- cash/views.py
+++ cash/views.py
@@ -1,26 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework.generics import ListCreateAPIView
 from django.views.generic import DetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .serializers import ExchangeSerializer
 from .models import Exchange, CurrencyRate, Case
 from .forms import *
 
 
-
 def index(request):
     return render(request, 'apps/index.html', {})
+
 
 @login_required
 def exchange_list(request):
     rates = Exchange.objects.filter(user=request.user).order_by('-id')
     return render(request, 'apps/index.html', {'rates': rates})
 
+
 class ExchangeCreateView(ListCreateAPIView):
     queryset = Exchange.objects.all()
     serializer_class = ExchangeSerializer
+
 
 #    def perform_create(self, serializer):
 #        serializer.save(user=self.request.user)
@@ -36,6 +40,7 @@ def posterview(request):
         'rates': rates,
         'cases': cases
     }
+
     return render(request, template_name, context)
 
 
